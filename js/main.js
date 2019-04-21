84721b6f8175b6e41db00c7b797133e268c22848
cyrus <48845648+chuneycu2@users.noreply.github.com>
2019-04-21 08:10:31 -0400

    jquery for slideToggle and logo img animations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(() => {
+  /* mobile nav dropdown */
   $('#menu-icon').on('click', () => {
-    $('#dropdown-nav').toggleClass('hide');
-  })
-
+    $('#dropdown-nav').slideToggle('fast', 'swing');
+  });
+  /* home button animation */
+  $("#home-button").mouseover(function() {
+    this.src="./resources/images/logo-icon-active.png"
+  }).mouseout(function() {
+    this.src="./resources/images/logo-icon.png"
+  });
+  /* footer home button animation */
+  $("#footer-button").mouseover(function() {
+    this.src="./resources/images/logo-icon-close-active.png"
+  }).mouseout(function() {
+    this.src="./resources/images/logo-icon-close.png"
+  });
 });
