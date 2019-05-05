943240f2b474d3b1eb69acb0fb42d2cc376c5f8d
cyrus <48845648+chuneycu2@users.noreply.github.com>
2019-05-05 14:15:48 -0400

    draft one for js functionality - hover tooltips, mobile menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
+/* jQuery */
+
 $(document).ready(() => {
   /* mobile nav dropdown */
   $('#menu-icon').on('click', () => {
-    $('#dropdown-nav').slideToggle('fast', 'swing');
+    $('.dropdown-nav').slideToggle('fast', 'swing');
+  });
+  /* project hover states */
+  $('#p1').hover(function() {
+    $('#t1').slideToggle('fast', 'swing');
+  });
+  $('#p2').hover(function() {
+    $('#t2').slideToggle('fast', 'swing');
+  });
+  $('#p3').hover(function() {
+    $('#t3').slideToggle('fast', 'swing');
+  });
+  $('#p4').hover(function() {
+    $('#t4').slideToggle('fast', 'swing');
+  });
+  $('#p5').hover(function() {
+    $('#t5').slideToggle('fast', 'swing');
+  });
+  $('#p6').hover(function() {
+    $('#t6').slideToggle('fast', 'swing');
   });
   /* home button animation */
   $("#home-button").mouseover(function() {
@@ -16,3 +37,22 @@ $(document).ready(() => {
     this.src="./resources/images/logo-icon-close.png"
   });
 });
+
+/* JS tab functionality */
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+/* 'clicks' the tab with ID 'defaultOpen' to make it appear on page load */
+document.getElementById("defaultOpen").click();
